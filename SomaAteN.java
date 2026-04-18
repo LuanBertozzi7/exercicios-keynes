1ebb9c4948bfd6afaa8208b95d9b39e1b8c441a3
LuanBertozzi7 <LuanBertozzi7@hotmail.com>
2026-04-18 14:46:54 -0400

    feat: SomaAteN

---
+++ SomaAteN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SomaAteN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N: ");
+        int n = sc.nextInt();
+        int soma = 0;
+        for (int i = 1; i <= n; i++) soma += i;
+        System.out.println("Soma: " + soma);
+        sc.close();
+    }
+}
